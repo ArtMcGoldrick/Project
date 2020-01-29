a129d2cadbe8529f48a061eee6192668644c7b60
S00186896 <S00186896@LAB-B1042-27>
2020-01-29 09:23:27 +0000

    Add project files.

---
+++ WpfApp1/Show.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Show
+    {
+        //Properties
+        public string ShowName { get; set; }
+
+        //Display with string
+        public override string ToString()
+        {
+            return $"{ShowName}";
+        }
+    }
+}
